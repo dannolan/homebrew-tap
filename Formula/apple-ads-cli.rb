6400cf0e2cf2d0c09467b08d1b166277c290ba42
Dan Nolan <dan@magicbrief.com>
2026-05-16 20:14:48 +1000

    Update apple-ads-cli to 0.1.5

--- Formula/apple-ads-cli.rb
+++ Formula/apple-ads-cli.rb
@@ -1,17 +1,17 @@
 class AppleAdsCli < Formula
   desc "Agent-first CLI for Apple Ads"
   homepage "https://github.com/dannolan/apple-ads-cli"
-  version "0.1.4"
+  version "0.1.5"
   license "MIT"
 
   depends_on :macos
 
   if Hardware::CPU.arm?
-    url "https://github.com/dannolan/apple-ads-cli/releases/download/v0.1.4/ads_darwin_arm64.zip"
-    sha256 "75b84532b673687086768ca7ad29fc2ccec5eb01485a6830f45a8a38f8d94dbd"
+    url "https://github.com/dannolan/apple-ads-cli/releases/download/v0.1.5/ads_darwin_arm64.zip"
+    sha256 "be352da04d8298a6e42b429a1a94259b41544cfa55f84be19ca666e83a126c82"
   else
-    url "https://github.com/dannolan/apple-ads-cli/releases/download/v0.1.4/ads_darwin_amd64.zip"
-    sha256 "b443369bf0cd53021bb1482bc9044d03545403601ed236ba118e8a21776a8e65"
+    url "https://github.com/dannolan/apple-ads-cli/releases/download/v0.1.5/ads_darwin_amd64.zip"
+    sha256 "64eb5fbff188e8abcec55865646b756af4fa6b149a12e43e53b5c5f5e127370b"
   end
 
   def install
